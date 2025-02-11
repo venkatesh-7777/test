0f24f8f0838059fff118b1be906b2e30567047dc
venkatesh-7777 <venkateshkarri854@gmail.com>
2025-02-11 11:31:13 +0530

    updated Todos app

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, title: "Go to gym", description: "Go to gym today" },
+    { id: 2, title: "Buy Horlicks", description: "Get Horlicks from the store" },
+    { id: 3, title: "Visit Ganesh", description: "Meet Ganesh in the evening" },
+  ]);
+
+  function addTodo() {
+    const newTodo = {
+      id: todos.length + 1, // Generate a unique ID
+      title: `Task ${todos.length + 1}`,
+      description: `Description for task ${todos.length + 1}`,
+    };
+
+    setTodos([...todos, newTodo]); // Correct way to update state
+  }
+
   return (
     <div>
-      <HeaderWithButton/>
-      <Header title={title} />
-      <Header title="venky2" />
+      <button onClick={addTodo}>Add Todo</button> {/* Button with text */}
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} description={todo.description} />
+      ))}
     </div>
   );
 }
 
-function HeaderWithButton(){
-  const [title, setTitle] = useState("my name is harkirat");
-  function updateTitle() {
-    setTitle("my name is " + Math.random());
-  }
-
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
 }
-
-function Header({ title }) {
-  return <div>{title}</div>;
-}
-
 export default App;
-
-
