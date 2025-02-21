048d40ea001297d9d4c39ea5ffbffb23d794b97a
venkatesh-7777 <venkateshkarri854@gmail.com>
2025-02-21 22:09:33 +0530

    wrappers

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
-import React, { useState } from "react";
+import React from 'react';
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, title: "Go to gym", description: "Go to gym today" },
-    { id: 2, title: "Buy Horlicks", description: "Get Horlicks from the store" },
-    { id: 3, title: "Visit Ganesh", description: "Meet Ganesh in the evening" },
-  ]);
-
-  function addTodo() {
-    const newTodo = {
-      id: todos.length + 1, // Generate a unique ID
-      title: `Task ${todos.length + 1}`,
-      description: `Description for task ${todos.length + 1}`,
-    };
-
-    setTodos([...todos, newTodo]); // Correct way to update state
-  }
-
   return (
     <div>
-      <button onClick={addTodo}>Add Todo</button> {/* Button with text */}
-      {todos.map((todo) => (
-        <Todo key={todo.id} title={todo.title} description={todo.description} />
-      ))}
+      <CardWrapper>
+        hi there
+      </CardWrapper>
     </div>
   );
 }
 
-function Todo({ title, description }) {
+function CardWrapper({ children }) {
+  console.log(children);
   return (
-    <div>
-      <h1>{title}</h1>
-      <h5>{description}</h5>
+    <div style={{ border: '2px solid black', padding: 20 }}>
+      {children}
     </div>
   );
 }
+
 export default App;
